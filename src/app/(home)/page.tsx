af32f152389151327e71df772ea64b8e4cb595c6
Sheel Taskar <sheeltaskar64@gmail.com>
2024-05-28 20:11:10 +0530

    Add footer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -16,6 +17,7 @@ export default function Home() {
       <div className="max-w-7xl mx-auto p-5 mt-20">
           <Skills />
           <Projects />
+          <Footer />
       </div>
 		</div>
 	);
